3f64077fdfc6c148a5dda02a20813621c0167021
Tomas Holicky <holickytomas@gmail.com>
2020-06-15 17:05:25 +0200

    Updated SPM

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
-
-// swift-tools-version:5.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "QExtensions",
+    platforms: [.macOS(.v10_12),
+    .iOS(.v11),
+    .tvOS(.v10),
+    .watchOS(.v3)],
     products: [
         .library(name: "QExtensions", targets: ["QExtensions"])
     ],
     targets: [
         .target(name: "QExtensions", path: "QExtensions")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
